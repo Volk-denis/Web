8f7a27610ae17efdf9b08567b9e8c59e0ad12d7a
batia-denis <batia-denis@outlook.com>
2018-09-29 21:23:39 +0300

    начало

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        WebContext webContext = new WebContext();
+
+        public ActionResult Index()
+        {
+            IEnumerable<Book> books = webContext.Books;
+            ViewBag.Books = books;
+
+            return View();
+        }
+
+        
+    }
+}
---
+++ Web/Models/WebContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class WebContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+    }
+
+    public class DbInicialaizer : DropCreateDatabaseAlways<WebContext>
+    {
+        protected override void Seed(WebContext context)
+        {
+            context.Books.Add(new Book { Id = 12, Name = "Л.Тостой" });
+            context.Books.Add(new Book { Id = 12, Name = "А.Пушкин" });
+
+            base.Seed(context);
+        }
+    }
+}
